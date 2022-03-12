c15fcbf644901b3ac2e18f37594c78f07f29480e
heitorAlbu <heitor.albuquerque@gmail.com>
2022-03-12 12:37:49 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Digite seu nome e pressione Enter");
+var nome = Console.ReadLine();
+//Console.Clear();
+
+
+Console.WriteLine("Agora digite seu Sobrenome :");
+var Sobrenome = Console.ReadLine();
+//Console.Clear();
+
+
+Console.WriteLine("Seu nome completo é : " + nome + " " + Sobrenome);
